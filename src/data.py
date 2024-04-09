ec85e82577e2424198e140cc95e22ea683984fbe
Joe Stella <Jastella18@gmail.com>
2024-04-08 20:34:41 -0400

    Update data.py

--- src/data.py
+++ src/data.py
@@ -5,11 +5,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from pybaseball import statcast
 import warnings
+from sklearn.preprocessing import LabelEncoder
 
 # Suppress FutureWarning from pybaseball
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-
+# Load the batter and pitcher stats from CSV files
 batter_stats = pd.read_csv("batterstats.csv")
 pitcher_stats = pd.read_csv("pitcherstats.csv")
 
@@ -18,12 +19,29 @@ start_date = "2023-01-01"
 end_date = "2023-12-31"
 statcast_data = statcast(start_dt=start_date, end_dt=end_date)
 
+# Filter the statcast data to only include occurrences where 'event' is not null
+statcast_data = statcast_data[statcast_data['events'].notnull()]
+
+# Merge the batter and pitcher stats with the statcast data using player_id
+merged_data = pd.merge(statcast_data, batter_stats, left_on='batter', right_on='player_id')
+merged_data = pd.merge(merged_data, pitcher_stats, left_on='pitcher', right_on='player_id')
+
+print("merge_data columns:", merged_data.columns)
+
 # Define features (batter and pitcher stats) and target variable (batter performance)
-X = batter_stats["woba"],pitcher_stats["woba"]  # Features
-y = statcast_data["events"]  # Target variable
+X = merged_data[['woba_x', 'woba_y']]  # Features
+y = merged_data['events']  # Target variable
+
+print(y)
+
+# Assuming 'events' is a categorical variable with the names of baseball plays
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+print("y_encoded:", y_encoded)
 
 # Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
 
 # Standardize features
 scaler = StandardScaler()
